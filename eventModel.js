7a03b925717c5520ad18dd5d4bc2c453ba8ce7a9
Nick Tamburro <Nick Tamburro>
2017-07-08 12:57:53 -0700

    adding a controllers folder

--- eventModel.js
+++ eventModel.js
@@ -2,7 +2,7 @@
 // The burger has a burger_name attribute of type DataTypes.String
 // and a devoured attribute that is false by default
 module.exports = function(sequelize, DataTypes) {
-  var Event = sequelize.define("Event", {
+  var Event = sequelize.define("Events", {
     Name: DataTypes.STRING,
     Description: DataTypes.STRING,
     Date: DataTypes.DATE
