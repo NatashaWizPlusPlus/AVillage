33ecf3d525c5c8ce0edd4b081ba6115101cfcd0c
chrisn27 <chris_nguyen27@yahoo.com>
2017-07-10 21:05:48 -0700

    HTML updates to creating an event and dynamically creating landin page and modals

--- public/javascript/app.js
+++ public/javascript/app.js
@@ -3,6 +3,7 @@ $(document).ready(function () {
   $('.modal').modal();
   $('#modal').modal('open');
   $('#modal').modal('close');
+  $('select').material_select();
 
   var eventItems = ["Chairs"];
 
@@ -17,23 +18,6 @@ $(document).ready(function () {
       $("#neededItems").append(itemButton);
     }
   }
-
-  $('.timepicker').pickatime({
-    default: 'now', // Set default time
-    fromnow: 0,       // set default time to * milliseconds from now (using with default = 'now')
-    twelvehour: true, // Use AM/PM or 24-hour format
-    donetext: 'OK', // text for done-button
-    cleartext: 'Clear', // text for clear-button
-    canceltext: 'Cancel', // Text for cancel-button
-    autoclose: false, // automatic close timepicker
-    ampmclickable: true, // make AM PM clickable
-    aftershow: function(){} //Function for after opening timepicker  
-  });
-
-  $('.datepicker').pickadate({
-    selectMonths: true, // Creates a dropdown to control month
-    selectYears: 15 // Creates a dropdown of 15 years to control year
-  });
 
 $("#category").change(function() {
     $('#category').text(this.val());
