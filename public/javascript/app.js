5d078e7315059c1e88fde4ad2f2aa908f7ad9eb3
chrisn27 <chris_nguyen27@yahoo.com>
2017-07-10 20:12:43 -0700

    homepage populating from sql

--- public/javascript/app.js
+++ public/javascript/app.js
@@ -18,6 +18,23 @@ $(document).ready(function () {
     }
   }
 
+  $('.timepicker').pickatime({
+    default: 'now', // Set default time
+    fromnow: 0,       // set default time to * milliseconds from now (using with default = 'now')
+    twelvehour: true, // Use AM/PM or 24-hour format
+    donetext: 'OK', // text for done-button
+    cleartext: 'Clear', // text for clear-button
+    canceltext: 'Cancel', // Text for cancel-button
+    autoclose: false, // automatic close timepicker
+    ampmclickable: true, // make AM PM clickable
+    aftershow: function(){} //Function for after opening timepicker  
+  });
+
+  $('.datepicker').pickadate({
+    selectMonths: true, // Creates a dropdown to control month
+    selectYears: 15 // Creates a dropdown of 15 years to control year
+  });
+
 $("#category").change(function() {
     $('#category').text(this.val());
 });
